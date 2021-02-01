52b8df6d8797a1e5400efc282e09f5b195b919e9
Abhishek Yadav <abhishekyad1098@gmail.com>
2021-02-01 17:57:15 -0500

    First commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,5 +20,8 @@ public class MainActivity extends AppCompatActivity {
                 (Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG)).show();
             }
         });
+
+
+        Log.d("Test", "This is a test!");
     }
 }
